be1e44171a4d73d23946051dabab70d6be6cc002
qinshengbo <1377270216@qq.com>
2018-12-10 09:12:10 +0800

    本地存储不完整修改

--- models/ysnn.js
+++ models/ysnn.js
@@ -2,6 +2,7 @@
 import { HTTP } from '../utils/http.js'
 
 // 通过extends继承父类（也可以实例化后调用）
+
 class IndexModel extends HTTP {
   getDataList(sCallBack) {
     //  封装后的请求调用
@@ -9,7 +10,8 @@ class IndexModel extends HTTP {
       url: 'classic/latest',
       success: (res) => {
         // console.log(res.index)
-        wx.setStorageSync(this._getKey(res.index), res)
+        let key = `classic-${res.index}`
+        wx.setStorageSync(key, res)
           
         sCallBack(res)
 
@@ -18,9 +20,9 @@ class IndexModel extends HTTP {
     // return  // return出结果会是空的，因为request是异步函数
   }
   //第一步：按照规则生成key值
-  _getKey(artIndex) {
-    let key = `classic-${artIndex}`
-    return key
-  }
+  // _getKey(artIndex) {
+  //   let key = `classic-${artIndex}`
+  //   return key
+  // }
 }
 export { IndexModel }
