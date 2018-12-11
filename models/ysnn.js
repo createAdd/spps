ea858c3b77abdd1a2830584dc1c1ccc28f725f50
unknown <liguodongw@hotmail>
2018-12-11 08:42:21 +0800

    音乐

--- models/ysnn.js
+++ models/ysnn.js
@@ -9,12 +9,9 @@ class IndexModel extends HTTP {
     this.request({
       url: 'classic/latest',
       success: (res) => {
-        // console.log(res.index)
         let key = `classic-${res.index}`
         wx.setStorageSync(key, res)
-          
         sCallBack(res)
-
       }
     })
     // return  // return出结果会是空的，因为request是异步函数
